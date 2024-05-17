3beb869c007d4bde9388cee8fa5f7257c488ad41
bminje <pmj3163@hufs.ac.kr>
2024-05-17 21:26:43 +0900

    Delete LanguageStyleDropdown.js

--- LanguageStyleDropdown.js
+++
@@ -1,9 +0,0 @@
-import { useState } from 'react';
-
-const useLanguageStyleState = () => {
-  const [languageStyle, setLanguageStyle] = useState('');
-
-  return { languageStyle, setLanguageStyle };
-};
-
-export default useLanguageStyleState;
